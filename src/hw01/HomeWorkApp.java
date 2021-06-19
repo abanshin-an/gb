b1067f062608cc730cc0e0d2447da33b2caf75e1
abanshin-an <abanshin-an@utg.gazprom.ru>
2021-06-19 19:03:21 +0400

    comment debug println

--- src/hw01/HomeWorkApp.java
+++ src/hw01/HomeWorkApp.java
@@ -17,7 +17,7 @@ public class HomeWorkApp {
     public static void checkSumSign() {
         int a = (int) (random() * 10 - 5);
         int b = (int) (random() * 10 - 5);
-        System.out.println("a+b= " + (a + b));
+//        System.out.println("a+b= " + (a + b));
         if ((a + b) >= 0) {
             System.out.println("Сумма положительная");
         } else
@@ -26,7 +26,7 @@ public class HomeWorkApp {
 
     private static void printColor() {
         int value = (int) (random() * 300 - 100);
-        System.out.printf("value= %d \n", value);
+//        System.out.printf("value= %d \n", value);
         if (value <= 0)
             System.out.println("Красный");
         else if (value <= 100)
@@ -38,7 +38,7 @@ public class HomeWorkApp {
     private static void compareNumbers() {
         int a = (int) (random() * 10 - 5);
         int b = (int) (random() * 10 - 5);
-        System.out.println("a= " + a + "; b= " + b);
+//        System.out.println("a= " + a + "; b= " + b);
         if (a >= b) {
             System.out.println("a >= b");
         } else {
